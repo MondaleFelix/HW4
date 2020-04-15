a42675fbbe2fba5deb6721a6b680d2b415072cb9
MondaleFelix <mondalefelix@yahoo.com>
2020-04-14 23:48:35 -0700

    added refactored code

--- coding_syntax.py
+++ coding_syntax.py
@@ -1,21 +1,51 @@
 # Problem: Given a list of n numbers, determine if it contains any duplicate numbers
 
 def find_dups(arry):
-	for i in range(len(aryy)):
+	for i in range(len(arry)):
 		for j in range(len(arry)):
 			if arry[i] == arry[j] and i is not j:
 				return True
+	return False
 
-find_dups(nums)
 
+dups_false = [1,2,3,4]
+# Returns False
+print(find_dups(dups_false))
 
+dups_true = [1,2,3,4,2]
+# Returns true
+print(find_dups(dups_true))
 
 # Find the middle item in a singly linked list, or two middle items if it contains an even number of nodes.
+
+class Node: 
+  
+    def __init__(self, data): 
+        self.data = data   
+        self.next = None   
+  
+  
+
+class LinkedList: 
+  
+    def __init__(self): 
+        self.head = None
+
+
 
 def find_middle_node(head):
 	first = head
 	second = head
-	While second.next is not None:
-		First = first.next
-		Second = second.next.next
-	Return first 
+	while second.next is not None:
+		first = first.next
+		second = second.next.next
+	return first.data
+
+
+a = Node("Mondale")
+b = Node("Is")
+c = Node("Cool")
+a.next = b
+b.next = c
+
+print(find_middle_node(a))
